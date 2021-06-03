ac56312889159b2d153214cdbf29bc205ecc145e
gxx <1152862327@qq.com>
2021-06-03 23:17:01 +0800

    ...

---
+++ src/chaptertwo/one/Shell.java
@@ -0,0 +1,25 @@
+package chaptertwo.one;
+
+/**
+ * @author gxx
+ * @create 2021-06-03 22:10
+ */
+public class Shell extends Example {
+
+    @Override
+    public void sort(Comparable[] a) {
+        int N = a.length;
+        int h = 1;
+        while (h < N/3) {
+            h = 3*h + 1;
+        }
+        while (h >= 1) {
+            for (int i = h; i < N; i++) {
+                for (int j = i; j >= h && less(a[j], a[j-h]); j-=h) {
+                    exch(a, j, j-h);
+                }
+            }
+            h = h / 3;
+        }
+    }
+}
--- src/chaptertwo/one/SortCompare.java
+++ src/chaptertwo/one/SortCompare.java
@@ -18,6 +18,9 @@ public class SortCompare {
         if (alg.equals("Selection")) {
             new Selection().sort(a);
         }
+        if (alg.equals("Shell")) {
+            new Shell().sort(a);
+        }
         return  timer.elapsedTime();
     }
 
@@ -36,11 +39,29 @@ public class SortCompare {
     public static void main(String[] args) {
         String alg1 = "Insertion";
         String alg2 = "Selection";
-        int N = 1000;
-        int T = 100;
-        double t1 = timeRandomInput(alg1, N, T);
-        double t2 = timeRandomInput(alg2, N, T);
-        StdOut.printf("For %d random Doubles\n  %s is", N, alg1);
-        StdOut.printf(" %.1f times faster than %s\n", t2/t1, alg2);
+        String alg3 = "Shell";
+        int N = 100000;
+        int T = 1;
+
+//        double t1 = timeRandomInput(alg1, N, T);
+//        System.out.println("Insertion = " + t1);
+//        double t2 = timeRandomInput(alg2, N, T);
+//        System.out.println("Selection = " + t2);
+//        StdOut.printf("For %d random Doubles\n  %s is", N, alg1);
+//        StdOut.printf(" %.1f times faster than %s\n", t2/t1, alg2);
+
+//        double t3 = timeRandomInput(alg3, N, T);
+//        System.out.println("Shell = " + t3);
+//        double t4 = timeRandomInput(alg1, N, T);
+//        System.out.println("Insertion = " + t4);
+//        StdOut.printf("For %d random Doubles\n  %s is", N, alg1);
+//        StdOut.printf(" %.1f times faster than %s\n", t4/t3, alg2);
+
+        double t5 = timeRandomInput(alg3, N, T);
+        System.out.println("Shell = " + t5);
+        double t6 = timeRandomInput(alg2, N, T);
+        System.out.println("Selection = " + t6);
+        StdOut.printf("For %d random Doubles\n  %s is", N, alg3);
+        StdOut.printf(" %.1f times faster than %s\n", t6/t5, alg2);
     }
 }
