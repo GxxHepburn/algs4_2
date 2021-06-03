7e94aa21ca974344a5088eca16060050b930f3ba
gxx <1152862327@qq.com>
2021-06-04 01:01:05 +0800

    ..

---
+++ src/chaptertwo/one/K2.java
@@ -0,0 +1,15 @@
+package chaptertwo.one;
+
+/**
+ * @author gxx
+ * @create 2021-06-04 0:53
+ */
+public class K2 {
+
+    public static void main(String[] args) {
+        /**
+         *  选择排序中，一个元素最多可能被交换   N 次
+         *  平均可能被交换 1 次
+         */
+    }
+}
