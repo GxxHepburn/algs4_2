787bef423af61abed3a74fb05a3a3155278c20ad
gxx <1152862327@qq.com>
2021-06-04 19:43:34 +0800

    ...

---
+++ src/chaptertwo/one/K4.java
@@ -0,0 +1,23 @@
+package chaptertwo.one;
+
+/**
+ * @author gxx
+ * @create 2021-06-04 19:36
+ */
+public class K4 {
+    /**
+     *  E   A   S   Y   Q   U   E   S   T   I   O   N
+     *
+     *  A   E   S   Y   Q   U   E   S   T   I   O   N
+     *  A   E   S   Y   Q   U   E   S   T   I   O   N
+     *  A   E   S   Y   Q   U   E   S   T   I   O   N
+     *  A   E   Q   S   Y   U   E   S   T   I   O   N
+     *  A   E   Q   S   U   Y   E   S   T   I   O   N
+     *  A   E   E   Q   S   U   Y   S   T   I   O   N
+     *  A   E   E   Q   S   S   U   Y   T   I   O   N
+     *  A   E   E   Q   S   S   T   U   Y   I   O   N
+     *  A   E   E   I   Q   S   S   T   U   Y   O   N
+     *  A   E   E   I   O   Q   S   S   T   U   Y   N
+     *  A   E   E   I   N   O   Q   S   S   T   U   Y
+     */
+}
