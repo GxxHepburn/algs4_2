e53d011a5394a09ed749483172a9f1bc605fce38
gxx <1152862327@qq.com>
2021-06-05 12:43:44 +0800

    ...

--- src/chaptertwo/one/K6.java
+++ src/chaptertwo/one/K6.java
@@ -1,5 +1,8 @@
 package chaptertwo.one;
 
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stopwatch;
+
 /**
  * 在主键都重复的情况下，插入排序更快
  *  1、逐渐都重复，所以无论是插入排序还是选择排序，都不交换元素，所以只考虑比较次数
@@ -12,4 +15,25 @@ package chaptertwo.one;
  * @create 2021-06-04 23:25
  */
 public class K6 {
+
+    public static void main(String[] args) {
+        int N = 150000;
+        Integer[] a = new Integer[N];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = 0;
+        }
+
+        Stopwatch timer_Insertion = new Stopwatch();
+        Example insertion = new Insertion();
+        insertion.sort(a);
+        double t_Insertion = timer_Insertion.elapsedTime();
+
+        Stopwatch timer_Selection = new Stopwatch();
+        Example selection = new Selection();
+        selection.sort(a);
+        double t_Selection = timer_Selection.elapsedTime();
+
+        StdOut.println("t_Insertion: " + t_Insertion + " , t_Selection: " + t_Selection
+                + " , ratio: " + t_Selection/t_Insertion);
+    }
 }
