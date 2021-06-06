cbfee4fefea19690f68a6276e88fb333e5104136
gxx <1152862327@qq.com>
2021-06-07 03:10:00 +0800

    ...

---
+++ src/chaptertwo/one/K23.java
@@ -0,0 +1,14 @@
+package chaptertwo.one;
+
+/**
+ * @author gxx
+ * @create 2021-06-07 3:06
+ */
+public class K23 {
+
+    public static void main(String[] args) {
+        /**
+         * 纸牌排序可以使用Shell排序，选择排序，插入排序，冒泡排序等都可以
+         */
+    }
+}
