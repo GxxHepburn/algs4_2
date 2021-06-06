b2e444b8e881d9b9d5488c36945d6c2174257798
gxx <1152862327@qq.com>
2021-06-07 02:53:21 +0800

    ...

---
+++ src/chaptertwo/one/K20.java
@@ -0,0 +1,15 @@
+package chaptertwo.one;
+
+/**
+ * @author gxx
+ * @create 2021-06-07 2:24
+ */
+public class K20 {
+
+    public static void main(String[] args) {
+        /**
+         * 最好情况就是已排好序的情况。因为无论h为何值，h子数组均已有序，都不需要交换元素。
+         * 比较次数也是最少的。
+         */
+    }
+}
