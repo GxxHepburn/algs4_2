802400abf662a037845ab03798cbb8d2e9659495
gxx <1152862327@qq.com>
2021-06-06 22:48:19 +0800

    ...

---
+++ src/chaptertwo/one/K18.java
@@ -0,0 +1,14 @@
+package chaptertwo.one;
+
+/**
+ * @author gxx
+ * @create 2021-06-06 22:40
+ */
+public class K18 {
+
+    public static void main(String[] args) {
+        /**
+         * 见2.1.17题
+         */
+    }
+}
