2f3bc0135a51bfc6e0f1da3186825228b5f387eb
gxx <1152862327@qq.com>
2021-06-06 16:56:26 +0800

    ...

---
+++ src/chaptertwo/one/K15.java
@@ -0,0 +1,16 @@
+package chaptertwo.one;
+
+/**
+ * @author gxx
+ * @create 2021-06-06 16:50
+ */
+public class K15 {
+
+    public static void main(String[] args) {
+        /**
+         * 选择排序
+         * 交换（也就是 Exch() 方法）需要一个额外空间，这里的条件满足。
+         * 现在我们应该使交换次数最少，选择排序只需要 N 次交换，比插入排序平均 N^2/4 少（N > 2）。
+         */
+    }
+}
---
+++ src/chaptertwo/one/K16.java
@@ -0,0 +1,12 @@
+package chaptertwo.one;
+
+/**
+ * @author gxx
+ * @create 2021-06-06 15:58
+ */
+public class K16 {
+
+    public static void main(String[] args) {
+
+    }
+}
