348aa56704a59a2497f73f44ad2631be4909521d
gxx <1152862327@qq.com>
2021-06-15 03:42:41 +0800

    ...

--- src/chaptertwo/two/K1.java
+++ src/chaptertwo/two/K1.java
@@ -3,19 +3,22 @@ package chaptertwo.two;
 public class K1 {
 
     public static void main(String[] args) {
-        /**
-         *                      A   E   Q   S   U   Y   E   I   N   O   S   T
-         * merge(a, 0, 0, 1)    A   E
-         * merge(a, 2, 2, 3)            Q   S
-         * merge(a, 0, 1, 3)    A   E   Q   S
-         * merge(a, 4, 4, 5)                    U   Y
-         * merge(a, 6, 6, 7)                            E   I
-         * merge(a, 4, 5, 7)                    E   I   U   Y
-         * merge(a, 0, 3, 7)    A   E   E   I   Q   S   U   Y
-         * merge(a, 8, 8, 9)                                    N   O
-         * merge(a, 10, 10, 11)                                         S   T
-         * merge(a, 8, 9, 11)                                   N   O   S   T
-         * merge(a, 0, 5, 11)   A   E   E   I   N   O   Q   S   S   T   U   Y
+        /**             a[]                                                                 aux[]
+         *  A   E   Q   S   U   Y   |   E   I   N   O   S   T
+         *  A   E   Q   S   U   Y   |   E   I   N   O   S   T               A   E   Q   S   U   Y   |   E   I   N   O   S   T
+         *
+         *  A                                                                   E   Q   S   U   Y   |   E   I   N   O   S   T
+         *  A   E                                                                   Q   S   U   Y   |   E   I   N   O   S   T
+         *  A   E   E                                                               Q   S   U   Y   |       I   N   O   S   T
+         *  A   E   E   I                                                           Q   S   U   Y   |           N   O   S   T
+         *  A   E   E   I   N                                                       Q   S   U   Y   |               O   S   T
+         *  A   E   E   I   N   O                                                   Q   S   U   Y   |                   S   T
+         *  A   E   E   I   N   O   Q                                                   S   U   Y   |                   S   T
+         *  A   E   E   I   N   O   Q   S                                                   U   Y   |                   S   T
+         *  A   E   E   I   N   O   Q   S   S                                               U   Y   |                       T
+         *  A   E   E   I   N   O   Q   S   S   T                                           U   Y   |
+         *  A   E   E   I   N   O   Q   S   S   T   U                                           Y   |
+         *  A   E   E   I   N   O   Q   S   S   T   U   Y
          */
     }
 }
---
+++ src/chaptertwo/two/K2.java
@@ -0,0 +1,21 @@
+package chaptertwo.two;
+
+public class K2 {
+
+    public static void main(String[] args) {
+        /**
+         *                      A   E   Q   S   U   Y   E   I   N   O   S   T
+         * merge(a, 0, 0, 1)    A   E
+         * merge(a, 2, 2, 3)            Q   S
+         * merge(a, 0, 1, 3)    A   E   Q   S
+         * merge(a, 4, 4, 5)                    U   Y
+         * merge(a, 6, 6, 7)                            E   I
+         * merge(a, 4, 5, 7)                    E   I   U   Y
+         * merge(a, 0, 3, 7)    A   E   E   I   Q   S   U   Y
+         * merge(a, 8, 8, 9)                                    N   O
+         * merge(a, 10, 10, 11)                                         S   T
+         * merge(a, 8, 9, 11)                                   N   O   S   T
+         * merge(a, 0, 5, 11)   A   E   E   I   N   O   Q   S   S   T   U   Y
+         */
+    }
+}
